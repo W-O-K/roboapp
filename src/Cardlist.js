00ef8943e8f5a458109b170395571c561ff6943a
W-O-K <wojiakor@gmail.com>
2020-05-17 18:26:31 +0100

    First commit

---
+++ src/Cardlist.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+const Card = ({ name, email, id }) => {
+	return(
+		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+			<div>
+			<h2>{name}</h2>
+			<p>{email}</p>
+			</div>
+		</div>
+		);
+}
+
+
+const Cardlist = ({ robots }) => {
+	
+	return(
+	<div>
+	   {
+	   	robots.map((user, i) => {
+	   		return (
+	   			  <Card
+	   				 key={robots[i].id}
+	   				 id={robots[i].id}
+	   				 name = {robots[i].name}
+	   				 email={robots[i].email}
+	   				 />
+	   			     );
+	   			})
+	   }
+    </div>
+		);
+}
+
+export default Cardlist;
+
